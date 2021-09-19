6f8c18dd18e10e25f4004449b8651e790e34e5ce
Vidit-Jain <viditjain@live.com>
2021-09-19 15:13:30 +0530

    Added arrow key implementation

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include "source/commands/history.h"
 #include "source/processor/execute.h"
 #include "source/processor/prompt.h"
@@ -11,19 +12,55 @@ int main() {
 	String *input = newString();
 	TokenArray *tokens = newTokenArray();
 	while (1) {
+        resetHistoryIndex();
+        setbuf(stdout, NULL);
+        enableRawMode();
 		printPrompt();
 		input->length = 0;
 		char inputCharacter;
-		while (1) {
-			inputCharacter = (char)getchar();
-			if (inputCharacter == '\n')
-				break;
-
-			input->str[input->length] = inputCharacter;
-			input->length++;
-		}
-		input->str[input->length] = '\0';
-		addHistory(input);
-		executeLine(tokens, *input);
-	}
+        while (read(STDIN_FILENO, &inputCharacter, 1) == 1) {
+            if (iscntrl(inputCharacter)) {
+                if (inputCharacter == 10) {
+                    printf("\n");
+                    break;
+                }
+                if (inputCharacter == 27) {
+                    char buffer[3];
+                    buffer[2] = 0;
+                    if (read(STDIN_FILENO, buffer, 2) == 2) {
+                        if (buffer[1] == 'A'){
+                            upArrow(input);
+                        }
+                        else if (buffer[1] == 'B') {
+                            downArrow(input);
+                        }
+                    }
+                } else if (inputCharacter == 127) {
+                    if (input->length > 0) {
+                        if (input->str[input->length - 1] == 9) {
+                            for (int i = 0; i < 7; i++) printf("\b");
+                        }
+                        input->str[--input->length] = '\0';
+                        printf("\b \b");
+                    }
+                } else if (inputCharacter == 9) {
+                    input->str[input->length++] = inputCharacter;
+                    for (int i = 0; i < 8; i++)
+                        printf(" ");
+                } else if (inputCharacter == 4) {
+                    exit(0);
+                } else {
+                    printf("%c\n", inputCharacter);
+                }
+            }
+            else {
+                input->str[input->length++] = inputCharacter;
+                printf("%c", inputCharacter);
+            }
+            }
+            disableRawMode();
+            input->str[input->length] = '\0';
+            addHistory(input);
+            executeLine(tokens, *input);
+        }
 }
